76414957ee8a8e969e5a08c53e81d4553b02111d
Murzav <mv@bel.net.ua>
2013-11-06 09:13:16 +0200

    Classes

--- php/client/jsonatpclient.php
+++ php/client/jsonatpclient.php
@@ -1,7 +1,11 @@
 <?php
 /**
+ * DevGar
+ * Ukraine, Odessa
  * Created by PhpStorm.
- * User: Администратор
+ * User: Victor Murzak
+ * email: mv@bel.net.ua
+ * GitHub: https://github.com/DevGarage/JSON-ATP.git
  * Date: 06.11.13
  * Time: 9:11
  */
