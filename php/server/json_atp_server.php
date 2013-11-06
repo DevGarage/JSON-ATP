14ab2071eb7dba52f9c2f0cbc5d685fcbf18a154
theSiMj <thesimj@gmail.com>
2013-11-06 10:40:01 +0200

    Start! Class create!

--- php/server/json_atp_server.php
+++ php/server/json_atp_server.php
@@ -3,8 +3,8 @@
  * DevGar
  * Ukraine, Odessa
  * Created by PhpStorm.
- * User: Victor Murzak
- * email: mv@bel.net.ua
+ * User: Bubelich Nikolay
+ * email: thesimj@gmail.com
  * GitHub: https://github.com/DevGarage/JSON-ATP.git
  * Date: 06.11.13
  * Time: 9:06
